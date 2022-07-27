2f590c7c727bc54d12753d6e1d6a0b31a7449339
admin <admin@TeamCity>
2022-07-27 03:44:28 +0000

    TeamCity change in 'Maven_test_manual' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,98 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2022.04"
+
+project {
+
+    vcsRoot(Backend)
+
+    buildType(RenameArtifact)
+    buildType(BuildBackend)
+}
+
+object BuildBackend : BuildType({
+    name = "build backend"
+
+    artifactRules = "target/*.jar => build_result"
+
+    vcs {
+        root(Backend)
+    }
+
+    steps {
+        maven {
+            name = "build"
+            goals = "clean install"
+            runnerArgs = "-DskipTests=true"
+        }
+    }
+})
+
+object RenameArtifact : BuildType({
+    name = "rename artifact"
+
+    artifactRules = "result/*.jar"
+
+    steps {
+        script {
+            scriptContent = """
+                #!/bin/bash
+                mkdir result
+                mv build_result/backend-todo-list-0.0.1-SNAPSHOT.jar result/todo-%build.counter%.jar
+            """.trimIndent()
+        }
+    }
+
+    triggers {
+        finishBuildTrigger {
+            buildType = "${BuildBackend.id}"
+            successfulOnly = true
+        }
+    }
+
+    dependencies {
+        dependency(BuildBackend) {
+            snapshot {
+            }
+
+            artifacts {
+                artifactRules = "build_result => build_result"
+            }
+        }
+    }
+})
+
+object Backend : GitVcsRoot({
+    name = "backend"
+    url = "git@github.com:PingWIn87/backend-todo-list-maven.git"
+    branch = "maven"
+    authMethod = uploadedKey {
+        uploadedKey = "github"
+    }
+})
